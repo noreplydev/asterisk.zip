1162c93c0a5037e9f9874703f6a97240f3ecdb49
noreplydev <contact.cristiansanchez@gmail.com>
2024-12-01 22:07:04 +0100

    Add background video to the root page

--- apps/view/components/root/page.tsx
+++ apps/view/components/root/page.tsx
@@ -1,7 +1,14 @@
 import { Risk } from "../Risk";
 
 export default function Page() {
-  return <div className="relative h-screen w-screen flex flex-col justify-center items-center">
+  return <div className="relative h-screen w-screen flex flex-col justify-center items-center
+    overflow-hidden">
     <Risk />
+    <div className="h-screen w-screen grid place-items-center">
+      <video src="/move_in_silence.webm" autoPlay muted loop
+        controls={false}
+        className="opacity-20 w-screen h-screen object-cover" />
+      <source />
+    </div>
   </div>
 }
