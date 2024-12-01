81aa5ef09a7690572b363088ff5fa8a837f38d9a
noreplydev <contact.cristiansanchez@gmail.com>
2024-12-01 20:42:22 +0100

    Remove deprecated code from api app

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,27 +1,27 @@
 import fs from 'fs'
 import express from 'express'
 import dotenv from 'dotenv'
-import config from '../../../bucket.config.js'
-import { Storage } from '@root/packages/persistence'
+// import config from '../../../bucket.config.js'
+// import { Storage } from '@root/packages/persistence'
 
 
-dotenv.config({ path: '../../.env' });
-const app = express()
-const bucketPath = process.argv[2] ?? process.cwd()
-console.log('process', process.env.STORAGE_ACCESS_KEY)
-const storage = new Storage(process.env.STORAGE_ACCESS_KEY, process.env.STORAGE_SECRET_KEY, false)
+// dotenv.config({ path: '../../.env' });
+// const app = express()
+// const bucketPath = process.argv[2] ?? process.cwd()
+// console.log('process', process.env.STORAGE_ACCESS_KEY)
+// const storage = new Storage(process.env.STORAGE_ACCESS_KEY, process.env.STORAGE_SECRET_KEY, false)
 
-app.get("/entries", (req, res) => {
-    const entries = fs.readdirSync(bucketPath, { withFileTypes: true })
-    const augmentedEntries = entries.map(entry => {
-        return {
-            name: entry.name,
-            type: entry.isDirectory() ? 'folder' : 'file'
-        };
-    });
-    res.send(augmentedEntries)
-})
+// app.get("/entries", (req, res) => {
+//     const entries = fs.readdirSync(bucketPath, { withFileTypes: true })
+//     const augmentedEntries = entries.map(entry => {
+//         return {
+//             name: entry.name,
+//             type: entry.isDirectory() ? 'folder' : 'file'
+//         };
+//     });
+//     res.send(augmentedEntries)
+// })
 
-app.listen(config.BUCKET_PORT, () => {
-    console.log('Bucket listening on http://localhost:' + config.BUCKET_PORT)
-})
+// app.listen(config.BUCKET_PORT, () => {
+//     console.log('Bucket listening on http://localhost:' + config.BUCKET_PORT)
+// })
