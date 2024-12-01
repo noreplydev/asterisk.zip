dfa1718841e2b19a782814869e166505baf3cf68
noreplydev <contact.cristiansanchez@gmail.com>
2024-12-01 21:06:34 +0100

    Add risk to the home page

---
+++ apps/view/components/root/page.tsx
@@ -0,0 +1,7 @@
+import { Risk } from "../Risk";
+
+export default function Page() {
+  return <div className="relative h-screen w-screen flex flex-col justify-center items-center">
+    <Risk />
+  </div>
+}
