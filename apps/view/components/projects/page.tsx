52d62a295f5acbb76897db86311a6e0d8692f0aa
noreplydev <contact.cristiansanchez@gmail.com>
2024-12-06 14:41:29 +0100

    Update projects page

--- apps/view/components/projects/page.tsx
+++ apps/view/components/projects/page.tsx
@@ -11,13 +11,13 @@ export default function Page() {
   ]
 
   return <div className="relative h-fit w-screen flex flex-col justify-start items-center
-    overflow-y-scroll gap-5">
+    overflow-y-scroll gap-24 py-28">
     {
       projects.map((project, index) => {
         return <div
           key={project.src}
-          className={" w-1/3 transition-all ease-in-out bg-red-500 overflow-hidden"
-          }
+          className="w-1/2 md:w-1/3 lg: 1/3 xl:1/3 transition-all ease-in-out bg-red-500 overflow-hidden
+            shadow-lg shadow-[#1c1c1c81]"
         >
           <Cover
             className="h-full w-full"
