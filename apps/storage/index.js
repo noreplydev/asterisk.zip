a27659791cced9c1944b9c645eb7972d927c9fd2
noreplydev <contact.cristiansanchez@gmail.com>
2024-12-08 00:53:26 +0100

    Add storage app credentials management

--- apps/storage/index.js
+++ apps/storage/index.js
@@ -5,11 +5,9 @@ dotenv.config({ path: '../../.env' });
 
 const MINIO_EXECUTABLE = 'minio'; // assumes minio binary is on the $path 
 const MINIO_DATA_DIR = process.env.STORAGE_DATA_DIR ?? "~/";
-const MINIO_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY;
-const MINIO_SECRET_KEY = process.env.STORAGE_SECRET_KEY;
 
-process.env.MINIO_ACCESS_KEY = MINIO_ACCESS_KEY;
-process.env.MINIO_SECRET_KEY = MINIO_SECRET_KEY;
+process.env.MINIO_ROOT_USER = process.env.STORAGE_ROOT_USER ?? "changeme"
+process.env.MINIO_ROOT_PASSWORD = process.env.STORAGE_ROOT_PASSWORD ?? "changeme"
 
 
 const minioProcess = spawn(MINIO_EXECUTABLE, ['server', MINIO_DATA_DIR]);
