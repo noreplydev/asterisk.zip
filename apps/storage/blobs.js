aef618416770360c75bc26fcc5c0c04c8b4117a2
noreplydev <contact.cristiansanchez@gmail.com>
2024-12-08 13:59:05 +0100

    Refactor storage starting process

--- apps/storage/blobs.js
+++ apps/storage/blobs.js
@@ -2,7 +2,7 @@ import { runCommand } from './index.js';
 
 export function RunBlobDatabase() {
   const MINIO_EXECUTABLE = 'minio'; // assumes minio binary is on the $PATH
-  const MINIO_DATA_DIR = process.env.STORAGE_DATA_DIR ?? "~/";
+  const MINIO_DATA_DIR = process.env.STORAGE_BLOB_DIR ?? "~/";
   process.env.MINIO_ROOT_USER = process.env.STORAGE_ROOT_USER ?? "changeme";
   process.env.MINIO_ROOT_PASSWORD = process.env.STORAGE_ROOT_PASSWORD ?? "changeme";
 
--- apps/storage/documents.js
+++ apps/storage/documents.js
@@ -6,24 +6,38 @@ export function RunDocumentDatabase() {
   const MONGO_PORT = process.env.MONGO_PORT ?? "27017";
   const MONGO_INITDB_ROOT_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME ?? "admin";
   const MONGO_INITDB_ROOT_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD ?? "password";
+  const MONGO_VOLUME_PATH = process.env.STORAGE_DOCUMENT_DIR
 
-  try {
-    // Check if the container exists
-    const existingContainer = execSync(`docker ps -aq -f name=${MONGO_CONTAINER_NAME}`).toString().trim();
-    if (existingContainer) {
-      console.log(`Removing existing container with name "${MONGO_CONTAINER_NAME}"`);
-      execSync(`docker rm -f ${MONGO_CONTAINER_NAME}`);
-    }
-  } catch (error) {
-    console.error('Error checking/removing existing container:', error.message);
+  if (!MONGO_VOLUME_PATH) {
+    console.error("STORAGE_DOCUMENT_DIR envar not provided.")
+    process.exit(1)
   }
 
-  runCommand('docker', [
-    'run', // if you want to run mongodb in detached mode uncomment this: '-d',
-    '--name', MONGO_CONTAINER_NAME,
-    '-p', `${MONGO_PORT}:27017`,
-    '-e', `MONGO_INITDB_ROOT_USERNAME=${MONGO_INITDB_ROOT_USERNAME}`,
-    '-e', `MONGO_INITDB_ROOT_PASSWORD=${MONGO_INITDB_ROOT_PASSWORD}`,
-    'mongo'
-  ], 'MongoDB');
+  try {
+    const containerExists = execSync(
+      `docker ps -a -q -f name=^/${MONGO_CONTAINER_NAME}$`
+    ).toString().trim();
+
+    if (containerExists) {
+      console.log(`Starting container with name: "${MONGO_CONTAINER_NAME}"`);
+      runCommand('docker', [
+        'start',
+        '-ai', // attach STDOUT/STDERR and STDIN open
+        MONGO_CONTAINER_NAME
+      ], 'MongoDB');
+    } else {
+      console.log(`Creating and starting container with name: "${MONGO_CONTAINER_NAME}"`);
+      runCommand('docker', [
+        'run',
+        '--name', MONGO_CONTAINER_NAME,
+        '-p', `${MONGO_PORT}:27017`,
+        '-e', `MONGO_INITDB_ROOT_USERNAME=${MONGO_INITDB_ROOT_USERNAME}`,
+        '-e', `MONGO_INITDB_ROOT_PASSWORD=${MONGO_INITDB_ROOT_PASSWORD}`,
+        '-v', `mongo-data:${MONGO_VOLUME_PATH}`,
+        'mongo'
+      ], 'MongoDB');
+    }
+  } catch (error) {
+    console.error('Error gestionando el contenedor de MongoDB:', error.message);
+  }
 }
