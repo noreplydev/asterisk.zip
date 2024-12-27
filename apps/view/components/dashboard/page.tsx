df8a69b1b7ff2651cdd84d04ab39c54db96aadd5
noreplydev <contact.cristiansanchez@gmail.com>
2024-12-27 12:01:26 -0800

    handle projects fetching errors

--- apps/view/components/dashboard/page.tsx
+++ apps/view/components/dashboard/page.tsx
@@ -8,9 +8,12 @@ const fetchProjects = async (fetch: any) => {
 }
 
 export default async function Page() {
-  let projects = await fetchProjects(fetch)
+  let projects: any = {};
+  try {
+    projects = await fetchProjects(fetch)
+  } catch (err) { console.error(err) }
 
-  if (!projects.success || !projects.projects) {
+  if (!projects?.success || !projects?.projects) {
     return <div className="relative h-fit w-screen flex flex-col justify-start items-center
     overflow-y-scroll gap-24 py-28">
       <h1>Error while loading projects</h1>
@@ -21,7 +24,7 @@ export default async function Page() {
     overflow-y-scroll gap-24 py-28">
     <h1>Projects</h1>
     {
-      projects.projects.map((project: any) => {
+      projects?.projects.map((project: any) => {
         const key = JSON.stringify(project)
         return <div key={key}>{JSON.stringify(project)}</div>
       })
